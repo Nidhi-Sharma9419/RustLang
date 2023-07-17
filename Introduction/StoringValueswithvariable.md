d1540bd93d167ecf8612d5466b4960331c4ff4f8
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-07-17 23:34:53 +0530

    Basics

---
+++ Introduction/StoringValueswithvariable.md
@@ -0,0 +1,16 @@
+create a variable to store the user input, like this:
+
+    let mut guess = String::new();
+ We use the let statement to create the variable. Here’s another example:
+
+    let apples = 5;
+
+In Rust, variables are immutable by default.    
+
+let apples = 5; // immutable
+let mut bananas = 5; // mutable
+
+String::new, a function that returns a new instance of a String. 
+The :: syntax in the ::new line indicates that new is an associated function of the String type.
+
+
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+// io stands for input/output library which comes from standard library
+//Rust has a set of items defined in the standard library that it brings into the scope of every program. This set is called the prelude.
+//If a type you want to use isn’t in the prelude, you have to bring that type into scope explicitly with a use statement.
+//Exactly like I did here to bring "io"
 fn main() {
-    println!("Hello, world!");
+//The fn syntax declares a new function;
+
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
