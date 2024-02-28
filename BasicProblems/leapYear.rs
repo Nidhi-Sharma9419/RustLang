a586889ad56ba412e39d19e6005a31ba8c8c0e32
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2024-02-28 10:07:37 +0530

    Taking Input in leapYear

--- BasicProblems/leapYear.rs
+++ BasicProblems/leapYear.rs
@@ -1,3 +1,17 @@
+//use rand::Rng;
+//use std::cmp::Ordering;
+use std::io;
 fn main(){
-    
+    //Taking input from user 
+    println!("Please input year");
+    let mut year = String::new();
+    io::stdin()
+    .read_line(&mut year)
+    .expect("Failed to read line");
+
+    // let year: u32 = match year.trim().parse() {
+    //     Ok(num) => num,
+    //     Err(_) => continue,
+    // }
+    println!("You guessed: {}", year);
 }
